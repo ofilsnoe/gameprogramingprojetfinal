1711ea8141653458d1d10c156ed8f45d471eb82e
ofilsnoe <filsnoe.ondoella@ynov.com>
2025-05-05 13:11:00 +0200

    gameprograming

---
+++ Assets/PacManmovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PacManMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Vitesse de déplacement
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = moveDirection * moveSpeed;
+    }
+}
